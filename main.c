c198b021c7e34eb0e78f813c526e32e847acc5ec
jc <jcgremateus@gmail.com>
2023-03-08 16:42:14 +0000

    KUGUI

--- main.c
+++ main.c
@@ -37,37 +37,35 @@ int main(int argc, char *argv[ ])
     FD_ZERO(&inputs); // Clear inputs
     FD_SET(0,&inputs); // Set standard input channel on
     //My variables
-    char user_input[20];
+    char user_input[12];
     int net, id;
-    char com[6];
+    char com[5];
 
     while(1)
     {
         testfds=inputs;
+        
         out_fds=select(FD_SETSIZE,&testfds,(fd_set *)NULL,(fd_set *)NULL,(struct timeval *) NULL);
-        switch(out_fds)
-        {
-            case 0:
-                printf("\n ---------------Timeout event-----------------\n");
-                break;
-            case -1:
-                perror("select");
+        
+        if(out_fds==-1){
+            perror("select");
                 exit(1);
-            default:
-                if(FD_ISSET(0,&testfds))
+        }
+        else{
+            if(FD_ISSET(0,&testfds))
                 {
-                    fgets(user_input, 12, stdin);
-                    sscanf(user_input,"%s %d %d",com,&net,&id);
-                    printf("\n%s, %d,%d\n",com,net,id);
-                    memset(user_input, 0, sizeof(user_input));
+                    fgets(user_input, 20, stdin);
+                    sscanf(user_input,"%s %d %d\n",com,&net,&id);
+                    printf("%s,%d,%d\n",com,net,id);
                 }
         }
+        
     }
     
     return 0;
 }
 
 int join_node(){
-    
+    //REG net id ip_da_maquina portaTCP
     return 0;
 }
